e49ab5dfe282bbadf34c3f871b6c0214f19b260d
ozapo <ozabdullah91@gmail.com>
2020-11-02 15:53:54 -0600

    DependencyInjection-ContructorInjection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/abdullah/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/abdullah/services/Java.java
@@ -1,10 +1,19 @@
 package com.abdullah.services;
 
 import com.abdullah.interfaces.Course;
+import com.abdullah.interfaces.ExtraSessions;
 
 public class Java implements Course {
+
+    //OfficeHours officeHours;
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : 20");
+        System.out.println("Weekly teaching hours : "+(20 + extraSessions.getHours()));
     }
 }
