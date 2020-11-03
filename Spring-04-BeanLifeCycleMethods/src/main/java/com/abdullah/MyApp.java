04d8284b7ff857560fc1d2ba653fe6bd0fd260a7
ozapo <ozabdullah91@gmail.com>
2020-11-02 18:11:23 -0600

    BeanScope-Singleton-Prototype

---
+++ Spring-04-BeanLifeCycleMethods/src/main/java/com/abdullah/MyApp.java
@@ -0,0 +1,21 @@
+package com.abdullah;
+
+import com.abdullah.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
+
+        // destroy method calling
+        ((ClassPathXmlApplicationContext)container).close();
+
+    }
+}
