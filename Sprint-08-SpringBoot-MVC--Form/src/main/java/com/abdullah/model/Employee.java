e71b92a5ebd609c978e11407be45b39167fb1da7
ozapo <ozabdullah91@gmail.com>
2020-11-18 20:29:56 -0600

    MVC Form EmployeeController added

---
+++ Sprint-08-SpringBoot-MVC--Form/src/main/java/com/abdullah/model/Employee.java
@@ -0,0 +1,30 @@
+package com.abdullah.model;
+
+
+import lombok.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    private String firtsName;
+    private String lastName;
+    private String birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
+
+
+
+
+}
